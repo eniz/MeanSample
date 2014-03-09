062e40655ae00d34c11b03f2a7625b948ecaf641
Eniz Gülek <enizgulek@gmail.com>
2014-03-09 22:40:37 +0200

    App name changed

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('yeniKlasorApp', [
+angular.module('sampleApp', [
   'ngCookies',
   'ngResource',
   'ngSanitize',
--- app/scripts/services/session.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('meanSampleApp')
-  .factory('Session', function ($resource) {
-    return $resource('/api/session/');
-  });
